c5531137d9a1810651fd762f0f41478bfac126de
ImranDoet <imranalaiz@hotmail.com>
2019-07-20 02:24:46 +0200

    Forgot this.. sigh

--- src/main/java/me/imrandoet/spidercore/api/module/Module.java
+++ src/main/java/me/imrandoet/spidercore/api/module/Module.java
@@ -1,11 +1,23 @@
 package me.imrandoet.spidercore.api.module;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-public abstract class Module {
+public abstract class Module<T extends JavaPlugin> {
+
+    private final T plugin;
+
+    public Module(T plugin) {
+        this.plugin = plugin;
+    }
 
     public abstract void run();
+
+    public T getInstance() {
+        return plugin;
+    }
 
     @Target(ElementType.TYPE)
     public @interface ModulePriority {
