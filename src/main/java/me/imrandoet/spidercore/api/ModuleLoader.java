fbb8710dc26d29a63462c3157ceb83ee1d5b4c11
ImranDoet <imranalaiz@hotmail.com>
2019-08-02 23:47:34 +0200

    New version: - Now sorted on Annotation!
    Signed-off-by: ImranDoet <imranalaiz@hotmail.com>

--- src/main/java/me/imrandoet/spidercore/api/ModuleLoader.java
+++ src/main/java/me/imrandoet/spidercore/api/ModuleLoader.java
@@ -2,13 +2,15 @@ package me.imrandoet.spidercore.api;
 
 import me.imrandoet.spidercore.api.module.IDisableable;
 import me.imrandoet.spidercore.api.module.Module;
+import me.imrandoet.spidercore.api.module.Module.ModulePriority;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -18,12 +20,14 @@ public class ModuleLoader<T> {
     private final T javaPlugin;
 
     private final Map<Class, Module> modules;
+    private final LinkedHashMap<Class, Module> sortedModules;
 
     private final String pluginPackage;
 
     public ModuleLoader(T javaPlugin, String pluginPackage) {
         this.javaPlugin = javaPlugin;
-        this.modules = new HashMap<>();
+        this.modules = new LinkedHashMap<>();
+        this.sortedModules = new LinkedHashMap<>();
         this.pluginPackage = pluginPackage;
 
         loadModules();
@@ -38,16 +42,14 @@ public class ModuleLoader<T> {
 
                 module.getEvents().forEach(o -> Bukkit.getPluginManager().registerEvents((Listener) o, (JavaPlugin) javaPlugin));
 
-                module.run();
-//                if (module instanceof IEvent) {
-//                    Bukkit.getPluginManager().registerEvents((IEvent) module, (JavaPlugin) javaPlugin);
-//                }
-
             } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException exception) {
                 //Custom logging system
                 exception.printStackTrace();
             }
         });
+        modules.entrySet().stream().sorted(Comparator.comparingInt(o -> ((ModulePriority) o.getKey().getAnnotation(ModulePriority.class)).priority())).forEachOrdered(classModuleEntry -> sortedModules.put(classModuleEntry.getKey(), classModuleEntry.getValue()));
+
+        this.sortedModules.entrySet().stream().forEach(classModuleEntry -> classModuleEntry.getValue().run());
     }
 
     public void disableModules() {
